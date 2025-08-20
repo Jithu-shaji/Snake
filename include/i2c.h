214b193e621d53f537c5a027fdf2986dade0731a
Jithu-shaji <jithu_shaji@outlook.com>
2025-08-20 18:37:50 +0530

    Added MACROS for status values. i2cStop is written as Macro function

--- include/i2c.h
+++ include/i2c.h
@@ -1,8 +1,19 @@
 #ifndef I2C_H
-#define I2C_H
+#define I2C_H   
+
+/*Setting register values for transmitting I2C STOP condition*/
+#define I2C_STOP()   (TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN))
+        
+/*0x18: SLA+W transmitted, ACK received*/        
+#define ADDR_ACK 0x18
+/*0x28: Data transmitted, ACK received*/
+#define DATA_ACK 0x28
+/*0x20: SLA+W transmitted, NACK received*/
+#define ADDR_NACK 0x20
+/*0x30: Data transmitted, NACK receive*/
+#define DATA_NACK 0x30
 
 uint8_t i2cStart();
-void i2cStop();
 uint8_t i2cWrite(uint8_t data);
 void i2cRead(uint8_t data);
 
--- src/i2c.c
+++ src/i2c.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <avr/io.h>
+#include <avr/wdt.h>
 #include "i2c.h"
 
 uint8_t i2cStart()
@@ -23,16 +24,10 @@ uint8_t i2cStart()
     return retVal;
 
 }
-void i2cStop()
-{
-    /*Setting register values for transmitting I2C STOP condition*/
-    TWCR = (1 << TWINT) | (1<<TWSTO) | (1<<TWEN);    
-
-}
 
 uint8_t i2cWrite(uint8_t data)
 {
-    uint8_t Status;
+    uint8_t Status = 0;
     uint8_t retVal = 0;
     /*Loading Data to be transmitted to TWDR register*/
     TWDR = data;
@@ -42,16 +37,12 @@ uint8_t i2cWrite(uint8_t data)
     while (!(TWCR & (1<<TWINT)));
     /*Masking TWSR register Prescalar to get status data*/
     Status = TWSR & 0xF8;
-    if((0x18 == Status) || (0x28 == Status))
+    if((ADDR_ACK == Status) || (DATA_ACK == Status))
     {
-        // 0x18: SLA+W transmitted, ACK received
-        // 0x28: Data transmitted, ACK received
         retVal = 1;
     }         
-    else if((0x20 == Status) || (0x30 == Status))
+    else if((ADDR_NACK == Status) || (DATA_NACK == Status))
     {
-        // 0x20: SLA+W transmitted, NACK received
-        // 0x30: Data transmitted, NACK received
         retVal = 2;
     }
     else
